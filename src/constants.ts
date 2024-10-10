75ed909872d4bdce7fad9fb75113f1c910174c9d
Ilya Venski <vensilya@gmail.com>
2024-10-10 12:52:27 +0200

    Better explanation for my metrics

--- src/constants.ts
+++ src/constants.ts
@@ -3,33 +3,37 @@ export const METRICS = [
     id: 1,
     emoji: '🍟',
     title: 'No fast food',
+    reasons:
+      "Better skin, better look, less fat, don't damage your health. Helps loose weight.",
     description: 'Can eat fast food once a week',
   },
   {
     id: 2,
     emoji: '🏃',
     title: 'Do sport',
-    description: 'Do sport every day',
+    reasons: 'Feel better during the days, get very fit, loose weight.',
+    description: 'Do sport 6 times a week. 1 day for restoring.',
   },
   {
     id: 3,
     emoji: '🚫🥙',
     title: 'Fasting',
-    description: 'IF 16:8 6 days a week',
+    description:
+      'Intermittent fasting at least 14h, but strive for 16h. And sometime longer fasting(24h, 36h). Can skip once a week.',
   },
   {
     id: 4,
     emoji: '📚',
     title: 'Reading',
     description: 'Read at least 10/15 minutes a day',
-  }
+  },
 ];
 
 export const START_DAY = '2024-10-06';
 
 export const DAY_STATE_COLORS = {
-  'failed': '#ff99a9',
-  'bellow50': '#c8f9cf',
-  'above50': '#80ed99',
-  'success': '#21ad70',
-}
+  failed: '#ff99a9',
+  bellow50: '#c8f9cf',
+  above50: '#80ed99',
+  success: '#21ad70',
+};
